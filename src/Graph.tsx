e56c70b8a6aeab14dc0e440a149da1e1e6738bcb
codershona <falgunimst95@gmail.com>
2020-07-02 15:43:09 +0200

    working on graph.tsx file

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -14,9 +14,11 @@ interface IProps {
  * Perspective library adds load to HTMLElement prototype.
  * This interface acts as a wrapper for Typescript compiler.
  */
-interface PerspectiveViewerElement {
-  load: (table: Table) => void,
-}
+ interface PerspectiveViewerElement {
+ interface PerspectiveViewerElement extends HTMLElement {
+    load: (table: Table) => void,
+  }
+
 
 /**
  * React component that renders Perspective based on data
@@ -31,6 +33,7 @@ class Graph extends Component<IProps, {}> {
   }
 
   componentDidMount() {
+    console.log('rendering');
     // Get element to attach the table from the DOM.
     const elem: PerspectiveViewerElement = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
 
@@ -41,16 +44,27 @@ class Graph extends Component<IProps, {}> {
       timestamp: 'date',
     };
 
-    if (window.perspective && window.perspective.worker()) {
-      this.table = window.perspective.worker().table(schema);
-    }
+      if (window.perspective && window.perspective.worker()) {
+     if (window.perspective) {
+       this.table = window.perspective.worker().table(schema);
+         }
     if (this.table) {
+      console.log('change table');
       // Load the `table` in the `<perspective-viewer>` DOM reference.
 
       // Add more Perspective configurations here.
       elem.load(this.table);
+      elem.setAttribute('view', 'y_line');
+    elem.setAttribute('column-pivots', '["stock"]');
+     elem.setAttribute('row-pivots', '["timestamp"]');
+   elem.setAttribute('columns', '["top_ask_price"]');
+    elem.setAttribute('aggregates', '{"stock":"distinct count","top_ask_price":"avg","top_bid_price":"avg","timestamp":"distinct count"}');
+
     }
   }
+
+
+
 
   componentDidUpdate() {
     // Everytime the data props is updated, insert the data into Perspective table
